060776cb014ad59696a3223ebba967ae28f962a4
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-07-06 19:58:01 +0530

    Add solution: Binary Tree Right Side View

---
+++ 199-binary-tree-right-side-view/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>result;
+    void solve(TreeNode* root, int level)
+    {
+        if( root== NULL) return;
+
+        if(result.size() < level)
+        {
+            result.push_back(root->val);
+        }
+        solve(root->right, level+1);
+        solve(root->left, level+1);
+
+        
+
+
+    }
+    vector<int> rightSideView(TreeNode* root) { 
+        if(root == NULL) return result;
+        solve(root, 1);
+        return result;
+        
+    }
+};
