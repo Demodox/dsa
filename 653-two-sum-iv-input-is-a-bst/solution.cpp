35c54e04d8a51d09ac9abd5e0b17acbb46b05ff4
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-07-24 13:11:09 +0530

    Add solution: Two Sum IV - Input is a BST

---
+++ 653-two-sum-iv-input-is-a-bst/solution.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, int k, set<int>& st, bool& ans )
+    {
+        if(root == NULL) return;
+
+        if(st.find(k- root->val) != st.end())
+        {
+            ans = true;
+            return;
+        }
+        solve(root->left, k, st,ans);
+        st.insert(root->val);
+
+        solve(root->right, k, st,ans);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        set<int>st;
+        bool ans = false;
+        solve( root, k, st,ans);
+        return ans;
+
+    }
+};
