04cab2ef5686512c9f6a3a6ba47b21512206e324
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-07-24 18:42:51 +0530

    Add solution: Two Sum IV - Input is a BST

--- 653-two-sum-iv-input-is-a-bst/solution.cpp
+++ 653-two-sum-iv-input-is-a-bst/solution.cpp
@@ -11,27 +11,21 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root, int k, set<int>& st, bool& ans )
+    void solve(TreeNode* root, int k, unordered_set<int>& st, bool& ans )
     {
         if(root == NULL) return;
-        if(k- root->val == 0)
-        {
-            ans =true;
-            return;
-        }
-
+       
         if(st.find(k- root->val) != st.end())
         {
             ans = true;
             return;
         }
-        solve(root->left, k, st,ans);
         st.insert(root->val);
-
+        solve(root->left, k, st,ans);
         solve(root->right, k, st,ans);
     }
     bool findTarget(TreeNode* root, int k) {
-        set<int>st;
+        unordered_set<int>st;
         bool ans = false;
         solve( root, k, st,ans);
         return ans;
