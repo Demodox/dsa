9fd784a1774de54da1003c879de221d7b4bfebf5
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-08-19 00:53:45 +0530

    Add solution: Max Consecutive Ones III

--- 1004-max-consecutive-ones-iii/solution.cpp
+++ 1004-max-consecutive-ones-iii/solution.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int result = 0;
-        int left=0; 
-        int right =0;
-        deque<int>dq;
-        while(right<nums.size())
+        int result =0;
+        int left =0;
+        
+        int countZero=0;
+
+        for(int right=0;right<nums.size();right++)
         {
-            if(nums[right] == 0)
+            if(nums[right] ==0) countZero++;
+
+            if(countZero > k)
             {
-                dq.push_back(right);
+                while(countZero>k && left<nums.size())
+                {
+                    if(nums[left] ==0) countZero--;
+                    left++;
+                }
             }
 
-            if(dq.size()>k)
-            {
-                left = dq.front() + 1;
-                dq.pop_front();
-            }
-            result = max(result,right - left +1);
-            right++;
+            result = max(result , right-left+1);
+
+
         }
         return result;
+        
     }
 };
