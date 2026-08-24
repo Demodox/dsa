8a4bf4ef47e612961483c23bc9c55144b4ee4fd8
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-08-24 23:28:35 +0530

    Add solution: Longest Palindromic Substring

--- 5-longest-palindromic-substring/solution.cpp
+++ 5-longest-palindromic-substring/solution.cpp
@@ -24,7 +24,7 @@ public:
 
         for(int i =0;i<n;i++)
         {
-            for(int j =i+1; j<n;j++)
+            for(int j =i; j<n;j++)
             {
                 if(isPalindrome(s, i , j))
                 {
