8ab6e4e42519ceaaf3b48459ed60755ca4a7bc5e
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-08-24 23:25:30 +0530

    Add solution: Longest Palindromic Substring

---
+++ 5-longest-palindromic-substring/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int  isPalindrome(string s, int i , int j)
+    {
+        if(i>=j) return 1;
+        if(dp[i][j] !=-1) return dp[i][j];
+
+        if(s[i] == s[j])
+        {
+            return dp[i][j]= isPalindrome(s, i+1,j-1);
+        }
+
+        return dp[i][j] =0;
+        
+    }
+    string longestPalindrome(string s) {
+
+        int n = s.length();
+        dp.resize(n, vector<int>(n, -1));
+        int length = 0;
+        string ans="";
+
+        for(int i =0;i<n;i++)
+        {
+            for(int j =i+1; j<n;j++)
+            {
+                if(isPalindrome(s, i , j))
+                {
+                    if( length < j-i+1)
+                    {
+                        length = j-i+1;
+                        ans=s.substr(i, j-i+1);
+                    }
+                }
+            }
+        }
+    
+        return ans;
+
+    }
+};
