a5cca5bcd9f590358545be0500dfa14819b61973
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-08-31 20:35:28 +0530

    Add solution: Partition Equal Subset Sum

--- 416-partition-equal-subset-sum/solution.cpp
+++ 416-partition-equal-subset-sum/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int totalsum =0;
-
+    vector<vector<int>>dp;
     bool solve(vector<int>& nums, int idx, int sum)
     {
-        if(idx>= nums.size()) return false;
+        if(idx>= nums.size() ) return false;
         if(sum == totalsum/2) return true;
 
+        if(dp[idx][sum] !=-1) return dp[idx][sum];
         bool take = solve(nums, idx+1, sum+nums[idx]);
         bool nottake = solve(nums, idx+1, sum);
 
-        return take || nottake;
+        return dp[idx][sum] = take || nottake;
 
     }
 
@@ -22,6 +23,8 @@ public:
         }
 
         if(totalsum  % 2 != 0) return false;
+
+        dp.resize(n+1, vector<int>(totalsum, -1));
 
         return solve(nums, 0,0);
         
