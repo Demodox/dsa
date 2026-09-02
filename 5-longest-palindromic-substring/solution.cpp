55b3c4973139794b7069c5edb2aabb2c17205ada
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-02 12:51:39 +0530

    Add solution: Longest Palindromic Substring

--- 5-longest-palindromic-substring/solution.cpp
+++ 5-longest-palindromic-substring/solution.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int  isPalindrome(string s, int i , int j)
+    int isPalindrome(string &s, int i, int j)
     {
-        if(i>=j) return 1;
-        if(dp[i][j] !=-1) return dp[i][j];
+        if(i>j) return 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
 
         if(s[i] == s[j])
         {
-            return dp[i][j]= isPalindrome(s, i+1,j-1);
+            return dp[i][j] =isPalindrome(s, i+1, j-1) ;
         }
+        return dp[i][j]=0;
 
-        return dp[i][j] =0;
-        
     }
     string longestPalindrome(string s) {
-
-        int n = s.length();
         
-        dp.resize(n+1, vector<int>(n+1, -1));
+        int n = s.length();
+        if(n==1)  return s;
+        dp.resize(n+1, vector<int>(n+1 , -1));
         int length = 0;
-        string ans=s.substr(0,1);
-
-        for(int i =0;i<n;i++)
+        string result = s.substr(0,1); 
+        for( int i =0;i<n;i++)
         {
-            for(int j =i+1; j<n;j++)
+            for(int j =i; j<n;j++)
             {
-                if(isPalindrome(s, i , j))
+                if(isPalindrome(s, i, j))
                 {
-                    if( length < j-i+1)
+                    if(length < j-i+1)
                     {
-                        length = j-i+1;
-                        ans=s.substr(i, j-i+1);
+                        length = j-i +1;
+                        result = s.substr(i,j-i+1);
                     }
                 }
             }
         }
-    
-        return ans;
-
+        return result;
     }
 };
