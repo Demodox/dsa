3f9af62d7d89a48a08e407c90584f89f58e5835b
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-02 20:04:58 +0530

    Add solution: Valid Anagram

---
+++ 242-valid-anagram/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t) :
+            return False
+            
+        freq = [0] * 26
+
+        for ch in s:
+            freq[ord(ch) - ord('a')] +=1
+        for ch in t:
+            freq[ord(ch) - ord('a')] -=1
+
+        for i in freq:
+            if i != 0: 
+                return False
+
+        return True
