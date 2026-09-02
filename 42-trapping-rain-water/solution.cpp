7b2ed7da66806b61625917d8ff42c19791cd3603
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-02 16:06:28 +0530

    Add solution: Trapping Rain Water

---
+++ 42-trapping-rain-water/solution.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int left = 0;
+        int right = n-1;
+        int leftMax =0;
+        int rightMax =0;
+         int result =0;
+
+        while(left<=right)
+        {
+            if(height[left] >=height[right])
+            {
+                if(height[right] < rightMax)
+                {
+                    result +=rightMax - height[right];
+                    
+                }
+                else
+                {
+                    rightMax = height[right];
+                }
+
+                right --;
+
+            }
+            else
+            {
+
+                if(height[left] >= leftMax)
+                {
+                    leftMax = height[left];
+                }
+                else
+                {
+                    result += leftMax - height[left];
+                }
+                
+                left++;
+            }
+            
+        }
+
+        return result;
+        
+    }
+};
