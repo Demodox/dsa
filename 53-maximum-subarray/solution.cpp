de6a139b606cea6418738edc3042d8ad9364ef8b
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-02 19:00:54 +0530

    Add solution: Maximum Subarray

--- 53-maximum-subarray/solution.cpp
+++ 53-maximum-subarray/solution.cpp
@@ -3,20 +3,22 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         int sum = nums[0];
-        int result  = sum;
+        int result  = nums[0];
         for(int i =1;i<n;i++)
         {
-            if(nums[i] + sum >=0)
+            
+            if(nums[i] + sum > nums[i])
             {
                 sum += nums[i];
-                result = max(sum,  result);
+                result = max(result,sum);
 
             }
             else
             {
-                result = max(sum,  result);
-                sum = 0;
+                sum = nums[i];
+                result = max(result,sum);
             }
+            
         }
         
         return result;
