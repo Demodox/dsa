05d1feedb0e04237351bc47c2ebddc12d81919ed
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-10 02:09:49 +0530

    Add solution: Max Consecutive Ones III

--- 1004-max-consecutive-ones-iii/solution.py
+++ 1004-max-consecutive-ones-iii/solution.py
@@ -1,26 +1,23 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-
         n = len(nums)
-        l = 0
-        countZero = 0
+        dq = deque()
+
         result = 0
+        l = 0
 
         for r in range(0,n):
-            if nums[r] == 0 :
-                countZero +=1
+            if nums[r] == 0:
+                dq.append(r)
+                
             
-            if countZero >k:
-                while countZero>k and l<n:
-                    if nums[l] == 0:
-                        countZero -=1
-                    
-                    l+=1
-            
-            result = max(result,r-l+1)
-
+            if len(dq) >k:
+                l =dq[0] +1
+                dq.popleft()
+                
+            result = max(result, r-l+1) 
+        
         return result
-    
 
 
         
