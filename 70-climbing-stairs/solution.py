b9a83f8bd5ca0550816386eccc9e54cfbe02b9ed
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-10 22:54:05 +0530

    Add solution: Climbing Stairs

--- 70-climbing-stairs/solution.py
+++ 70-climbing-stairs/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+
+        dp = [-1]* (n+1)
         def solve( n: int, steps :int)->int:
 
             if steps >n :
                 return 0
             if  steps == n :
                 return 1
+
+            if dp[steps] !=-1 :
+                return dp[steps]
             
             ways = solve(n, steps +2) + solve(n, steps +1)
+            dp[steps]= ways
             return ways
 
         result = solve(n, 0)
