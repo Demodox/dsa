b6c33714e5571edb0df7f7dbac2dafebaaabeb4a
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-11 00:11:00 +0530

    Add solution: Best Time to Buy and Sell Stock

---
+++ 121-best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        p =(float('inf'), -1)
+        q = (float('-inf'), -1)
+        result = 0
+
+        for i in range(0,n):
+        
+            if prices[i] < p[0] :
+                p =(prices[i], i)
+
+                if p[1] <q[1]:
+                    result = max(result , q[0] - p[0])
+            
+            elif prices[i] >  q[0] :
+                q =(prices[i], i)
+
+                if p[1] <q[1]:
+                    result = max(result , q[0] - p[0])
+        
+        return result
+
+
+
+            
+
+        
