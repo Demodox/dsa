d1db77736975839d7aa7136db51d9da3ca887e32
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-13 01:42:21 +0530

    Add solution: Maximum Subarray

---
+++ 53-maximum-subarray/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        sum = 0
+        count =0
+        result = nums[0]
+        for num  in nums :
+            if num + count >num :
+                count += num
+                result = max(result , count) 
+                 
+            else:
+                count = num
+        return result         
+        
