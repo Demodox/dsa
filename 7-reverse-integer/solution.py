0f7ba76f01251a1be8a0e2ca89ea2f1ef80052a1
Kuntal Mondal <131608254+Demodox@users.noreply.github.com>
2026-09-12 22:57:12 +0530

    Add solution: Reverse Integer

--- 7-reverse-integer/solution.py
+++ 7-reverse-integer/solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def reverse(self, x: int) -> int:
         result =0
+        INT_MAX = 2** 32 -1
+        INT_MIN = - 2**32
         id = 1
         if x<0:
             id =-1
@@ -10,5 +12,7 @@ class Solution:
             x = x//10
             result = result *10 + rem
 
+        if result * id <INT_MIN or result *id > INT_MAX:
+            return 0
         return result *id
         
